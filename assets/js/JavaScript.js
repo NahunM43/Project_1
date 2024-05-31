d0165ea3fd9f7c0aab3579954245c594cf6f884d
Alexander_L <alexander.w.leyva@gmail.com>
2024-05-30 19:38:19 -0500

    added fetch for edamam API

--- assets/js/JavaScript.js
+++ assets/js/JavaScript.js
@@ -1,2 +1,7 @@
-console.log(1)
-//hello
+fetch('https://api.edamam.com/doc/open-api/recipe-search-v2.json')
+.then(function(res) {
+  console.log(res);
+  return res.json();
+}) .then(function(data){
+  console.log("after json()", data)
+});
