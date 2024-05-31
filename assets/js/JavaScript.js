0c881753204537238a9458d93638d52841ab9d23
Alexander_L <alexander.w.leyva@gmail.com>
2024-05-30 19:03:04 -0500

    test

--- assets/js/JavaScript.js
+++ assets/js/JavaScript.js
@@ -1,13 +1,1 @@
-fetch('https://api.edamam.com/doc/open-api/recipe-search-v2.json')
-  .then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operatioin:', error);
-  });
+console.log(21)
